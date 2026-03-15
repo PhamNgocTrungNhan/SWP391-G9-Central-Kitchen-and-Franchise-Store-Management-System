af7c6612b97355367974775ad90a48e215fd98ca
Quan <Quanvonguyen2002@gmail.com>
2026-03-15 23:41:26 +0700

    Refactor Layout component: Add Sidebar visibility based on route and improve layout structure

--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import Sidebar from './Sidebar'
 
 export default function Layout() {
+    const location = useLocation()
+    const isDashboardRoute = location.pathname === '/dashboard'
+
     return (
         <div className="relative flex h-auto min-h-screen w-full flex-col bg-background-light dark:bg-background-dark font-display text-slate-900 dark:text-slate-100 overflow-x-hidden">
-            <div className="flex h-full grow flex-col">
-                <Outlet />
+            <div className="flex h-full grow">
+                {!isDashboardRoute && <Sidebar />}
+                <div className="flex h-full grow flex-col min-w-0">
+                    <Outlet />
+                </div>
             </div>
         </div>
     )
